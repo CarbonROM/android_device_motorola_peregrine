523a7b2cca463d1a330d07c3cd92b86f66fa9b25
Rabih Salamey <somcom3@carbonrom.org>
2015-03-07 11:37:09 -0600

    peregrine: update torwards different build enviornment
       * Omni calls things differently, adopt to that.
    
    Change-Id: I003743a0e3028cadc22622f35b57cabb70e81962

--- carbon_peregrine.mk
+++ carbon_peregrine.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+# Inherit some common CARBON stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
 PRODUCT_NAME := carbon_peregrine
