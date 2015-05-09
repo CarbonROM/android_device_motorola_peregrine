eda319ab40dd45ff638630c30a6077aeda9cc62b
STELIX <ssspinni@gmail.com>
2015-05-09 10:22:45 +0200

    Peregrine - Carbonize device
    Change-Id: Ic1b60e44e45d6be2f0dd7a72bb3efe7f212fb6db
    Signed-off-by: STELIX <ssspinni@gmail.com>

---
+++ carbon.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
+
+# Inherit some common CarbonROM stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+PRODUCT_RELEASE_NAME := peregrine
+PRODUCT_NAME := carbon_peregrine
